7ae543ba8b4e48b79ce4218b0dcb2a77aa8c3eeb
sandhani shaik <sanshaik@igategegdc.com>
2018-11-29 20:39:14 +0530

    sandhaniu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Aux from '../../hoc/Aux';
-import classNames from './Layout.css';
-import SearchBar from '../Navigation/SearchBar';
-const Layout = (props) =>(
-    <Aux>
-        <SearchBar />
-        <main className={classNames.Contant}>
-            {props.children}
-        </main> 
-    </Aux>
-)
+import Aux from "../../hoc/Aux";
+import classNames from "./Layout.css";
+import SearchBar from "../Navigation/SearchBar";
+const Layout = props => (
+  <Aux>
+    <SearchBar />
+    <main className={classNames.Contant}>
+      {props.children}
+      <div>Hiiii</div>
+    </main>
+  </Aux>
+);
 export default Layout;
